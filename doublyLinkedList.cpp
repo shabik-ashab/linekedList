6b16cfceb40b0ee081da5e0160ddd53153decfb2
nub69 <shabik.ashab@gmail.com>
2022-08-27 11:48:14 +0600

    insertAtHead,tail,display,countLength added

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -15,9 +15,61 @@ public:
     }
 };
 
+void insertAtTail(Node* &head, int val){
+    Node *newNode = new Node(val);
+
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+
+    Node *temp = head;
+    while(temp->Next != NULL){
+        temp = temp->Next;
+    }
+
+    temp->Next = newNode;
+    newNode->Prev = temp;    
+}
+
+void inserAtHead(Node* &head, int val){
+    Node* newNode = new Node(val);
+
+    head->Prev = newNode;
+    newNode->Next = head;
+    head = newNode;
+}
+
+void display(Node* n){
+    while(n != NULL){
+        cout<<n->value;
+        if(n->Next != NULL) cout<<" -> ";
+        n = n->Next;
+    }
+}
+
+int countLength(Node* &head){
+    int count = 0;
+    Node* temp = head;
+
+    while(temp != NULL){
+        count++;
+        temp = temp->Next;
+    }
+
+    return count;
+}
+
+
 
 int main()
 {
-    
+    Node* head = NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    inserAtHead(head,0);
+    insertAtTail(head,4);
+
     return 0;
 }
